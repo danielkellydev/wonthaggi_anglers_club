e1f322b768408dbfc62c82366838809f3e2c06bb
danfkelly <danielkellydev@gmail.com>
2026-03-30 10:58:08 +1100

    changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2026_03_29_233448) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "about_sections", force: :cascade do |t|
     t.string "title", null: false
     t.integer "position", default: 0, null: false
